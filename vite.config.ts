c7e7a4c2cc585ee38623cb03d014915db958bbd7
jinho-waah <andrewjo950515@gmail.com>
2025-02-28 19:41:31 +0900

    chore: not using github page

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/jongtobang/",
   define: {
     "process.env": {},
   },
