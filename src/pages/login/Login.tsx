ace63521dcb4d0dd95345755e0d5f0a1e0f8cb62
jinho-waah <andrewjo950515@gmail.com>
2025-03-01 21:53:39 +0900

    mod: change redirect uri

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -4,23 +4,27 @@ export default function Login() {
 
     switch (provider) {
       case "naver":
-        authUrl = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${
-          import.meta.env.VITE_NAVER_CLIENT_ID
-        }&redirect_uri=${
-          import.meta.env.VITE_NAVER_REDIRECT_URI
-        }&state=random_state`;
+        // authUrl = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${
+        //   import.meta.env.VITE_NAVER_CLIENT_ID
+        // }&redirect_uri=${
+        //   import.meta.env.VITE_NAVER_REDIRECT_URI
+        // }&state=random_state`;
+        authUrl = `https://jongtobang.duckdns.org/oauth2/authorization/naver`;
+
         break;
       case "kakao":
-        authUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${
-          import.meta.env.VITE_KAKAO_CLIENT_ID
-        }&redirect_uri=${import.meta.env.VITE_KAKAO_REDIRECT_URI}`;
+        // authUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${
+        //   import.meta.env.VITE_KAKAO_CLIENT_ID
+        // }&redirect_uri=${import.meta.env.VITE_KAKAO_REDIRECT_URI}`;
+        authUrl = `https://jongtobang.duckdns.org/oauth2/authorization/kakao`;
         break;
       case "google":
-        authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${
-          import.meta.env.VITE_GOOGLE_CLIENT_ID
-        }&redirect_uri=${
-          import.meta.env.VITE_GOOGLE_REDIRECT_URI
-        }&response_type=code&scope=email profile openid`;
+        // authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${
+        //   import.meta.env.VITE_GOOGLE_CLIENT_ID
+        // }&redirect_uri=${
+        //   import.meta.env.VITE_GOOGLE_REDIRECT_URI
+        //   }&response_type=code&scope=email profile openid`;
+        authUrl = `https://jongtobang.duckdns.org/oauth2/authorization/google`;
         break;
       default:
         return;
